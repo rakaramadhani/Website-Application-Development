d6fdd315af7e93ba5004d192ba05dbe487274b62
rakaramadhani <100126637+rakaramadhani@users.noreply.github.com>
2023-11-09 20:17:14 +0700

    delete.php

--- Latihan Database/Latihan 4/delete.php
+++ Latihan Database/Latihan 4/delete.php
@@ -6,9 +6,19 @@ $id = $_GET ["code_game"];
 
 
 if ( hapus($id) > 0) {
-
+    echo "
+    <script>
+        alert('Game berhasil dihapus!');
+        document.location.href = '../Latihan 1/connection.php';
+    </script>
+    ";
 } else {
-
+    echo "
+        <script>
+            alert('Game gagal di hapus!');
+            document.location.href = '../Latihan 1/connection.php';
+        </script>
+        ";
     
 }
 
